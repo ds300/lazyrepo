0b1d3ee0cee1adc2a450b426809e6f0efd0a7e0b
Mark <markladyshev@gmail.com>
2023-04-12 12:41:15 +0200

    adjust eslint configuration

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,9 +5,19 @@ module.exports = {
     node: true,
     'jest/globals': true,
   },
-  extends: ['eslint:recommended', 'plugin:n/recommended', 'plugin:@typescript-eslint/recommended'],
+  extends: ['eslint:recommended', 'plugin:n/recommended'],
   parser: '@typescript-eslint/parser',
   overrides: [
+    {
+      files: ['*.ts', '*.js'],
+      extends: [
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+      ],
+      parserOptions: {
+        project: ['./tsconfig.json'],
+      },
+    },
     {
       files: ['test/**'],
       plugins: ['jest'],
@@ -24,6 +34,8 @@ module.exports = {
     eqeqeq: ['error', 'always'],
 
     'n/no-process-exit': 'off',
+    'n/no-missing-import': 'off',
+    'n/no-missing-require': 'off',
 
     '@typescript-eslint/no-empty-function': ['error', { allow: ['arrowFunctions'] }],
   },
