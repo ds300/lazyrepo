6b856e489424c51cb6d1edcfa0faedaf3a1a0abb
David Sheldrick <d.j.sheldrick@gmail.com>
2023-04-14 09:29:59 +0100

    disable ban-ts-comment

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -14,6 +14,7 @@ module.exports = {
   },
   rules: {
     '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
     '@typescript-eslint/no-extra-semi': 'off',
     'n/no-missing-import': 'off',
     'n/no-process-exit': 'off',
