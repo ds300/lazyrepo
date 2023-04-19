cd0774727237bd5d8c65d258ff8b452eb7d9d903
David Sheldrick <d.j.sheldrick@gmail.com>
2023-04-19 12:41:56 +0100

    reinstate bin

--- bin.js
+++ bin.js
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 
-// import { resolve } from 'path'
-// import { existsSync } from './src/fs.js'
+import { resolve } from 'path'
+import { existsSync } from './src/fs.js'
 
-// if (existsSync('./node_modules/lazyrepo/src/cli.js')) {
-//   await import(resolve('./node_modules/lazyrepo/src/cli.js'))
-// } else {
-await import('./src/cli.js')
-// }
+if (existsSync('./node_modules/lazyrepo/src/cli.js')) {
+  await import(resolve('./node_modules/lazyrepo/src/cli.js'))
+} else {
+  await import('./src/cli.js')
+}
